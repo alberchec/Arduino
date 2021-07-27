136f9ccb74ec52b28b62edbe97a139d54124388d
alberchec <alberchec@hotmail.com>
2021-07-27 13:35:35 -0300

    add Blink_Led_Pot

---
+++ Blink_Led_Pot/Blink_Led_Pot.ino
@@ -0,0 +1,36 @@
+void check_pot(){
+  static int prev_pot_val = 250;
+  static bool led_state = HIGH;
+  int pot_val = analogRead(A0);
+  int pot_dif = pot_val - prev_pot_val;
+  if(pot_dif < 0 ) pot_dif *= -1;
+
+  static unsigned long last = 0;
+  const int period = 100;
+  if( (millis()-last) > period ){
+    if(pot_dif > 5){
+      prev_pot_val = pot_val;
+      led_state = !led_state;
+      last = millis();
+    }else if(!led_state){
+      led_state = HIGH;
+      last = millis();
+    }
+  }
+  //if( (millis()-last) > 500 ){
+    //if(pot_dif < 5) led_state = HIGH;
+  //}
+
+  if(led_state) digitalWrite(8,HIGH);
+  else          digitalWrite(8,LOW);
+}
+
+void setup() {
+  pinMode(A0,INPUT);
+  pinMode(8,OUTPUT);
+
+}
+
+void loop() {
+  check_pot();
+}
---
+++ Blink_Led_Pot/README.md
@@ -0,0 +1,3 @@
+# Blink Led with potentiometer
+
+This is a program that makes a Led blink when the potentiometer is turned.
