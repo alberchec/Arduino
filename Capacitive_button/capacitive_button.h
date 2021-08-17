c25c75ab83afb983ced9cfe08a5486241643fc01
alberchec <alberchec@hotmail.com>
2021-08-16 23:26:32 -0300

    add capacitive_button files

---
+++ Capacitive_button/capacitive_button.h
@@ -0,0 +1,24 @@
+#ifndef PUSH_BUTTON
+#define PUSH_BUTTON
+
+#include <CapacitiveSensor.h>
+
+class Capacitive_button {
+  private:
+    static const byte check_msec = 4;
+    static const byte press_msec = 20;
+    static const byte release_msec = 20;
+    static unsigned long interval;
+    static byte count;
+    bool debounced_state;
+    void debounce(bool&);
+    CapacitiveSensor* sensor;
+    int threshold;
+  public:
+    Capacitive_button(uint8_t,uint8_t,const int);
+    bool read();
+};
+unsigned long Capacitive_button::interval = 0;
+byte Capacitive_button::count = 2;
+
+#endif
