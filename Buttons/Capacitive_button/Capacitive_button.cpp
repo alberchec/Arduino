23e9f573c5f428ea83f8d2d649737b01dfed00da
alberchec <alberchec@hotmail.com>
2021-08-25 15:44:23 -0300

    changes

--- Buttons/Capacitive_button/Capacitive_button.cpp
+++ Buttons/Capacitive_button/Capacitive_button.cpp
@@ -1,42 +1,8 @@
 #include <Capacitive_button.h>
 
-Capacitive_button::Capacitive_button(uint8_t send_pin, uint8_t receive_pin,const int thrsld){
-  sensor = new CapacitiveSensor(send_pin,receive_pin);
-  threshold = thrsld;
-}
+Capacitive_button::Capacitive_button(uint8_t send_pin, uint8_t receive_pin,unsigned int thld) : Cap_sensor::Cap_sensor(send_pin,receive_pin,thld){}
 
 bool Capacitive_button::read(){
   raw_state = signal_processing();
-
   return Button_base::read();
-}
-
-void Capacitive_button::set_threshold(int thld){
-  threshold = thld;
-}
-
-bool Capacitive_button::signal_processing(){
-  const int dt = 2;
-  const int it = threshold; //around 500
-  const float leakage = 0.9;
-  long raw = sensor->capacitiveSensorRaw(30);
-  static long last_raw = raw;
-  static long last_integ = 0;
-
-  long dev = raw - last_raw;
-  long integ;
-
-  if(abs(dev) > dt){
-    integ = last_integ + dev;
-  }else{
-    integ = last_integ;
-  }
-  
-  if(integ > it){
-    //last_integ = integ;
-    return true;
-  }else{
-    last_integ = integ * leakage;
-  }
-  return false;
 }
--- Buttons/Capacitive_button/Capacitive_button.h
+++ Buttons/Capacitive_button/Capacitive_button.h
@@ -1,18 +1,13 @@
 #ifndef CAPACITIVE_BUTTON
 #define CAPACITIVE_BUTTON
 
-#include <CapacitiveSensor.h>
+#include <Cap_sensor.h>
 #include <Button_base.h>
 
-class Capacitive_button : public Button_base{
-  private:
-    CapacitiveSensor* sensor;
-    int threshold;
-    bool signal_processing();
+class Capacitive_button : public Button_base, public Cap_sensor{
   public:
-    Capacitive_button(uint8_t,uint8_t,const int);
+    Capacitive_button(uint8_t,uint8_t,unsigned int);
     bool read();
-    void set_threshold(int);
 };
 
 #endif
